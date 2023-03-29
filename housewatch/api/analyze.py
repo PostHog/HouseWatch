42e29b2b9c0b6d1cd0090e732e70fe2f2dfaa8c3
timgl <tim@posthog.com>
2023-03-28 22:09:20 -0400

    fxi

--- housewatch/api/analyze.py
+++ housewatch/api/analyze.py
@@ -2,6 +2,7 @@ from rest_framework.viewsets import GenericViewSet
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from housewatch.clickhouse.client import client
 
 class AnalyzeViewset(GenericViewSet):
     def list(self, request: Request) -> Response:
@@ -9,4 +10,13 @@ class AnalyzeViewset(GenericViewSet):
 
     @action(detail=False, methods=["GET"])
     def slow_queries(self, request: Request):
-        return Response([{'whatever': 'ok'}])
+        ddd = client.execute("""
+        SELECT toUInt8(type) as query_type, query, query_duration_ms 
+FROM clusterAllReplicas('posthog', system.query_log) 
+WHERE is_initial_query AND event_time > now() - INTERVAL 2 WEEK
+ORDER BY query_duration_ms DESC
+LIMIT 100
+""")
+
+    
+        return Response(ddd)
