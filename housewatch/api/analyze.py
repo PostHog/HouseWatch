d24c105d5c938dd762b599dbc39070707e5c8942
timgl <tim@posthog.com>
2023-03-28 22:36:25 -0400

    Fix queries

--- housewatch/api/analyze.py
+++ housewatch/api/analyze.py
@@ -3,7 +3,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from housewatch.clickhouse.client import run_query, base_params
-from housewatch.clickhouse.queries.sql import SLOW_QUERIES_SQL
+from housewatch.clickhouse.queries.sql import SLOW_QUERIES_SQL, SCHEMA_SQL, PAGE_CACHE_SQL
 
 class AnalyzeViewset(GenericViewSet):
     def list(self, request: Request) -> Response:
@@ -17,23 +17,10 @@ class AnalyzeViewset(GenericViewSet):
 
     @action(detail=False, methods=["GET"])
     def page_cache(self, request: Request):
-        ddd = run_query("""
-with 
-    toDateTime( now() ) as target_time, 
-    toIntervalDay( 14 ) as interval
-select
-    getMacro('replica') replica,
-    (sum(ProfileEvents['OSReadChars']) - sum(ProfileEvents['OSReadBytes'])) / sum(ProfileEvents['OSReadChars']) AS page_cache_read_ratio
-from clusterAllReplicas('posthog', system,query_log)
-where 
-    event_time >= target_time - interval 
-    and event_time <= target_time 
-    and type > 1 
-    and is_initial_query 
-    and JSONExtractString(log_comment, 'kind') = 'request'
-group by replica
-order by replica
-        """)
-
-    
+        ddd = run_query(PAGE_CACHE_SQL)
         return Response(ddd)
+
+    @action(detail=False, methods=["GET"])
+    def schema(self, request: Request):
+        query_result = run_query(SCHEMA_SQL)
+        return Response(query_result)
--- housewatch/clickhouse/queries/sql.py
+++ housewatch/clickhouse/queries/sql.py
@@ -6,3 +6,25 @@ SLOW_QUERIES_SQL = """
     ORDER BY query_duration_ms DESC
     LIMIT %(limit)s
 """
+
+SCHEMA_SQL = """
+SELECT name, formatReadableSize(total_bytes) as readable_bytes, total_bytes, total_rows FROM system.tables ORDER BY total_bytes DESC
+"""
+
+PAGE_CACHE_SQL = """
+with 
+    toDateTime( now() ) as target_time, 
+    toIntervalDay( 14 ) as interval
+select
+    getMacro('replica') replica,
+    (sum(ProfileEvents['OSReadChars']) - sum(ProfileEvents['OSReadBytes'])) / sum(ProfileEvents['OSReadChars']) AS page_cache_read_ratio
+from clusterAllReplicas('posthog', system,query_log)
+where 
+    event_time >= target_time - interval 
+    and event_time <= target_time 
+    and type > 1 
+    and is_initial_query 
+    and JSONExtractString(log_comment, 'kind') = 'request'
+group by replica
+order by replica
+"""
