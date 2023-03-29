97b743ca1a95f73aaaeb1020a0557484fc95a820
yakkomajuri <yakko.majuri@gmail.com>
2023-03-28 23:24:54 -0300

    page_cache template

--- housewatch/api/analyze.py
+++ housewatch/api/analyze.py
@@ -3,7 +3,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from housewatch.clickhouse.client import client, base_params
-from housewatch.clickhouse.queries.sql import SLOW_QUERIES_SQL
+from housewatch.clickhouse.queries.sql import SLOW_QUERIES_SQL, PAGE_CACHE_HIT_PERCENTAGE_SQL
 
 class AnalyzeViewset(GenericViewSet):
     def list(self, request: Request) -> Response:
@@ -13,29 +13,12 @@ class AnalyzeViewset(GenericViewSet):
     def slow_queries(self, request: Request):
         params = { **base_params, "limit": 100, "date_from": "now() - INTERVAL 2 WEEK"}
         query_result = client.execute(SLOW_QUERIES_SQL % params)
-
     
         return Response(query_result)
 
     @action(detail=False, methods=["GET"])
     def page_cache(self, request: Request):
-        ddd = client.execute("""
-with 
-    toDateTime( now() ) as target_time, 
-    toIntervalDay( 14 ) as interval
-select
-    getMacro('replica') replica,
-    (sum(ProfileEvents['OSReadChars']) - sum(ProfileEvents['OSReadBytes'])) / sum(ProfileEvents['OSReadChars']) AS page_cache_read_ratio
-from clusterAllReplicas('posthog', system,query_log)
-where 
-    event_time >= target_time - interval 
-    and event_time <= target_time 
-    and type > 1 
-    and is_initial_query 
-    and JSONExtractString(log_comment, 'kind') = 'request'
-group by replica
-order by replica
-        """)
-
+        params = { **base_params, "limit": 100, "date_to": "now()", "date_from": "now() - INTERVAL 2 WEEK"}
+        query_result = client.execute(PAGE_CACHE_HIT_PERCENTAGE_SQL % params)
     
-        return Response(ddd)
+        return Response(query_result)
--- housewatch/clickhouse/queries/sql.py
+++ housewatch/clickhouse/queries/sql.py
@@ -1,8 +1,26 @@
 # TODO: Add enum mapping dict for query `type`
 SLOW_QUERIES_SQL = """
-    SELECT toUInt8(type) as query_type, query, query_duration_ms 
-    FROM clusterAllReplicas(%(cluster)s, system.query_log) 
-    WHERE is_initial_query AND event_time > %(date_from)s
-    ORDER BY query_duration_ms DESC
-    LIMIT %(limit)s
+SELECT toUInt8(type) as query_type, query, query_duration_ms 
+FROM clusterAllReplicas(%(cluster)s, system.query_log) 
+WHERE is_initial_query AND event_time > %(date_from)s
+ORDER BY query_duration_ms DESC
+LIMIT %(limit)s
+"""
+
+PAGE_CACHE_HIT_PERCENTAGE_SQL = """
+WITH 
+    toDateTime(%(date_to)s) AS date_to, 
+    toDateTime(%(date_from)s) AS date_from, 
+SELECT
+    getMacro('replica') replica,
+    (sum(ProfileEvents['OSReadChars']) - sum(ProfileEvents['OSReadBytes'])) / sum(ProfileEvents['OSReadChars']) AS page_cache_read_ratio
+FROM clusterAllReplicas(%(cluster)s, system.query_log)
+WHERE 
+    event_time >= date_from
+    AND event_time <= date_to 
+    AND type > 1 
+    AND is_initial_query 
+    AND JSONExtractString(log_comment, 'kind') = 'request'
+GROUP BY replica
+ORDER BY replica
 """
