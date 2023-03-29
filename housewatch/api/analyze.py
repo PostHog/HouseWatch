f63f377895283d80675811956a895e3ea0d305e9
timgl <tim@posthog.com>
2023-03-29 13:53:19 -0400

    fix

--- housewatch/api/analyze.py
+++ housewatch/api/analyze.py
@@ -3,7 +3,9 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from housewatch.clickhouse.client import run_query, base_params
-from housewatch.clickhouse.queries.sql import SLOW_QUERIES_SQL, SCHEMA_SQL, PAGE_CACHE_SQL
+from housewatch.clickhouse.queries.sql import SLOW_QUERIES_SQL, SCHEMA_SQL, PAGE_CACHE_SQL, SLOW_QUERIES_BY_HASH_SQL, COLUMN_SIZE_SQL, QUERY_EXECUTION_COUNT_SQL
+
+DEFAULT_TIME = 24 * 7 * 2
 
 class AnalyzeViewset(GenericViewSet):
     def list(self, request: Request) -> Response:
@@ -12,8 +14,20 @@ class AnalyzeViewset(GenericViewSet):
     @action(detail=False, methods=["GET"])
     def slow_queries(self, request: Request):
         params = { **base_params, "limit": 100, "date_from": "now() - INTERVAL 2 WEEK"}
-        query_result = run_query(SLOW_QUERIES_SQL % params)
+        if request.GET.get('by_hash'):
+            query_result = run_query(SLOW_QUERIES_BY_HASH_SQL % params)
+        else:
+            query_result = run_query(SLOW_QUERIES_SQL % params)
         return Response(query_result)
+
+    @action(detail=True, methods=["GET"])
+    def query_detail(self, request: Request, pk: str):
+        hours = request.GET.get('hours', DEFAULT_TIME)
+        execution_count = run_query(QUERY_EXECUTION_COUNT_SQL.format(query_hash=pk, hours=hours))
+        return Response({
+            'execution_count': execution_count,
+
+        })
 
     @action(detail=False, methods=["GET"])
     def page_cache(self, request: Request):
@@ -23,4 +37,9 @@ class AnalyzeViewset(GenericViewSet):
     @action(detail=False, methods=["GET"])
     def schema(self, request: Request):
         query_result = run_query(SCHEMA_SQL)
+        return Response(query_result)
+
+    @action(detail=True, methods=["GET"])
+    def column_size(self, request: Request, pk: str):
+        query_result = run_query(COLUMN_SIZE_SQL)
         return Response(query_result)
--- housewatch/clickhouse/queries/sql.py
+++ housewatch/clickhouse/queries/sql.py
@@ -1,14 +1,30 @@
 # TODO: Add enum mapping dict for query `type`
 SLOW_QUERIES_SQL = """
-    SELECT toUInt8(type) as query_type, query, query_duration_ms 
+    SELECT toUInt8(type) as query_type, query, query_duration_ms, result_rows, formatReadableSize(result_bytes) as readable_bytes, toString(normalized_query_hash)
     FROM clusterAllReplicas(%(cluster)s, system.query_log) 
     WHERE is_initial_query AND event_time > %(date_from)s
     ORDER BY query_duration_ms DESC
     LIMIT %(limit)s
 """
 
+SLOW_QUERIES_BY_HASH_SQL = """
+    SELECT any(query), avg(query_duration_ms) avg_query_duration, avg(result_rows) avg_result_rows, formatReadableSize(avg(result_bytes)) as avg_readable_bytes, toString(normalized_query_hash)
+    FROM clusterAllReplicas(%(cluster)s, system.query_log) 
+    WHERE is_initial_query AND event_time > %(date_from)s
+    GROUP BY normalized_query_hash
+    ORDER BY avg_query_duration DESC
+    LIMIT %(limit)s
+"""
+
 SCHEMA_SQL = """
 SELECT name, formatReadableSize(total_bytes) as readable_bytes, total_bytes, total_rows FROM system.tables ORDER BY total_bytes DESC
+"""
+
+COLUMN_SIZE_SQL = """
+SELECT table, name as column, formatReadableSize(data_compressed_bytes), formatReadableSize(data_uncompressed_bytes) FROM system.columns
+WHERE table = 'sharded_events'
+GROUP BY table, column, data_compressed_bytes, data_uncompressed_bytes
+ORDER BY data_compressed_bytes DESC LIMIT 100
 """
 
 PAGE_CACHE_SQL = """
@@ -27,4 +43,23 @@ where
     and JSONExtractString(log_comment, 'kind') = 'request'
 group by replica
 order by replica
+"""
+
+QUERY_EXECUTION_COUNT_SQL = """
+SELECT
+    toUInt16(0) AS total,
+    toStartOfHour(now() - toIntervalHour(number)) AS day_start
+FROM numbers(dateDiff('hour', toStartOfHour(now()  - interval {hours} hour), now()))
+UNION ALL
+
+SELECT
+    count(1) as total,
+    toStartOfHour(query_start_time) as day_start
+FROM
+    clusterAllReplicas('posthog', system.query_log) 
+WHERE
+    toString(normalized_query_hash) = '{query_hash}' and
+    query_start_time > now() - interval {hours} hour
+GROUP BY toStartOfHour(query_start_time)
+ORDER BY toStartOfHour(query_start_time) DESC
 """
