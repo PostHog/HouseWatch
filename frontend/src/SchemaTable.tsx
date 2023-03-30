6da8e83cd49adba6b4ac91d2daa4641d969a5b20
timgl <tim@posthog.com>
2023-03-30 00:13:26 -0400

    merge

--- frontend/src/SchemaTable.tsx
+++ frontend/src/SchemaTable.tsx
@@ -3,17 +3,69 @@
 import * as React from 'react';
 import { usePollingEffect } from './PageCacheHits';
 import { DataGrid } from '@mui/x-data-grid';
+import { Treemap } from '@ant-design/charts';
 
+import {useHistory
+  } from "react-router-dom";
+
+
+
+function TableTreeMap({schema}) {
+  const history = useHistory()
+  
+
+  const config = {
+    data: {
+        name: "root",
+        children: schema.map(table => ({ name: table.column, value: table.compressed, ...table })),
+    },
+    colorField: 'name',
+    style: {cursor: 'pointer'},
+    label: {
+      style: {
+        fill: 'black',
+        fontSize: 14,
+        fontWeight: 600,
+        }
+    },
+    drilldown: {
+      enabled: true,
+      breadCrumb: {
+        rootText: 'Start over',
+      },
+    },
+    tooltip: {
+      formatter: (v) => {
+        const root = v.path[v.path.length - 1];
+        return {
+          name: v.name,
+          value: `${(v.value / 1000000).toFixed(2)}mb (percentage: ${((v.value / root.value) * 100).toFixed(2)}%)`,
+        };
+      },
+    },
+  }
+
+
+  return (
+    <div>
+      <Treemap {...config}   onEvent={(node, event) => {
+        if(event.type === 'element:click') {
+            history.push(`/schema/${event.data.data.name}`)
+        }
+      }} />
+    </div>
+  );
+}
 
 export default function CollapsibleTable({match}) {
 
-    const [slowQueries, setSlowQueries] = React.useState([]);
+    const [schema, setSchema] = React.useState([]);
 
 
     const url = `http://localhost:8000/api/analyze/${match.params.table}/schema`
 
     usePollingEffect(
-    async () => setSlowQueries(await fetch(url)
+    async () => setSchema(await fetch(url)
     .then(response => {
         return response.json()
     }
@@ -24,17 +76,18 @@ export default function CollapsibleTable({match}) {
     [],
     { interval: 3000 } // optional
     )
-    const slowQueriesCols = [
-        { field: 'column', headerName: 'Name', width: 100,},
-        { field: 'compressed', headerName: 'Compressed', width: 200},
-        { field: 'uncompressed', headerName: 'Uncompressed', width: 200}
+    const schemaCols = [
+        { dataIndex: 'column', title: 'Name' ,},
+        { dataIndex: 'compressed_readable', title: 'Compressed' },
+        { dataIndex: 'uncompressed', title: 'Uncompressed' }
     ]
   return (
     <div style={{ height: 800, width: '100%' }}>
         <h2>Table: {match.params.table}</h2>
+        {schema && <TableTreeMap schema={schema} />}
       <DataGrid
-        rows={slowQueries.map(d => ({id: d.column, ...d}))}
-        columns={slowQueriesCols}
+        rows={schema.map(d => ({id: d.column, ...d}))}
+        columns={schemaCols}
         pageSize={100}
         rowsPerPageOptions={[5]}
       />
