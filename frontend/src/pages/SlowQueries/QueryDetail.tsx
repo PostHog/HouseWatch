131eda6ef99f4fbc7bb1eabae4f31bc9e3580220
yakkomajuri <yakko.majuri@gmail.com>
2023-05-23 15:16:40 -0300

    fix: make normal deployment work, leave docker for later

--- frontend/src/pages/SlowQueries/QueryDetail.tsx
+++ frontend/src/pages/SlowQueries/QueryDetail.tsx
@@ -158,7 +158,7 @@ export default function QueryDetail({ match }: { match: { params: { query_hash: 
                                                 title={`Calculated from OSCPUVirtualTimeMicroseconds metric from ClickHouse query log's ProfileEvents.`}
                                             >
                                                 <span>
-                                                    <InfoCircleOutlined rev={undefined} />
+                                                    <InfoCircleOutlined />
                                                 </span>
                                             </Tooltip>
                                         </>
