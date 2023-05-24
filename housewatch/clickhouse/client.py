a2d999f42aa174c54ad9dc9b25199e0e24b0e0a1
yakkomajuri <yakko.majuri@gmail.com>
2023-05-24 15:31:52 -0300

    fix: default CLICKHOUSE_SECURE to true

--- housewatch/clickhouse/client.py
+++ housewatch/clickhouse/client.py
@@ -6,14 +6,14 @@ from housewatch.clickhouse.queries.sql import EXISTING_TABLES_SQL
 ch_host = os.getenv("CLICKHOUSE_HOST", "localhost")
 ch_verify = os.getenv("CLICKHOUSE_VERIFY", True)
 ch_ca = os.getenv("CLICKHOUSE_CA", None)
-ch_secure = os.getenv("CLICKHOUSE_SECURE", False)
+ch_secure = os.getenv("CLICKHOUSE_SECURE", True)
 
 pool = ChPool(
     host=ch_host,
     database=os.getenv("CLICKHOUSE_DATABASE", "default"),
     user=os.getenv("CLICKHOUSE_USER", "default"),
     password=os.getenv("CLICKHOUSE_PASSWORD", ""),
-    secure=ch_secure if ch_secure != '' else False,
+    secure=ch_secure if ch_secure != '' else True,
     ca_certs=ch_ca if ch_ca != '' else None,
     verify=ch_verify if ch_verify != '' else True,
     settings={"max_result_rows": "2000"}, 
