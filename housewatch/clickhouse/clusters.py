66652c888adea54bbdc797cff72fa4b81b15fc8f
James Greenhill <fuziontech@gmail.com>
2023-08-23 14:24:55 -0700

    add a bit of cluster magic

--- housewatch/clickhouse/clusters.py
+++ housewatch/clickhouse/clusters.py
@@ -3,7 +3,7 @@ from housewatch.clickhouse.client import run_query
 
 
 def get_clusters():
-    QUERY = """Select * FROM system.clusters"""
+    QUERY = """Select cluster, shard_num, shard_weight, replica_num, host_name, host_address, port, is_local, user, default_database, errors_count, slowdowns_count, estimated_recovery_time FROM system.clusters"""
     res = run_query(QUERY)
     clusters = defaultdict(list)
     for c_node in res:
@@ -15,5 +15,21 @@ def get_clusters():
 
 
 def get_cluster(cluster):
-    QUERY = """Select * FROM system.clusters WHERE cluster = '%(cluster_name)s' """
+    QUERY = """Select cluster, shard_num, shard_weight, replica_num, host_name, host_address, port, is_local, user, default_database, errors_count, slowdowns_count, estimated_recovery_time FROM system.clusters WHERE cluster = '%(cluster_name)s' """
     return run_query(QUERY, {"cluster_name": cluster})
+
+
+def get_shards(cluster):
+    cluster = get_cluster(cluster)
+    nodes = defaultdict(list)
+    for node in cluster:
+        nodes[node["shard_number"]].append(node)
+    return nodes
+
+
+def get_node_per_shard(cluster):
+    shards = get_shards(cluster)
+    nodes = []
+    for shard, n in shards.items():
+        nodes.append((shard, n[0]))
+    return nodes
