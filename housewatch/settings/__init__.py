08e98362ae464f49e98903dd93138cadbaf6461f
James Greenhill <fuziontech@gmail.com>
2024-11-02 06:49:28 -0600

    bugfix: Don't send traces to sentry from housewatch (#77)

--- housewatch/settings/__init__.py
+++ housewatch/settings/__init__.py
@@ -34,11 +34,11 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
+    traces_sample_rate=0.0,
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
+    profiles_sample_rate=0.0,
 )
 
 # TODO: Figure out why things dont work on cloud without debug
