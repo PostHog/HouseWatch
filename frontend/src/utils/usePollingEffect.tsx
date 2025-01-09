a624f067b2f43291a331dbf465b4bcdd71a29324
James Greenhill <fuziontech@gmail.com>
2025-01-09 11:59:53 -0800

    TS is not bad

--- frontend/src/utils/usePollingEffect.tsx
+++ frontend/src/utils/usePollingEffect.tsx
@@ -2,23 +2,23 @@ import { useEffect, useRef } from 'react'
 
 export function usePollingEffect(
     asyncCallback: any,
-    dependencies = [],
+    dependencies: any[] = [],
     {
         interval = 3000, // 3 seconds,
-        onCleanUp = () => {},
+        onCleanUp = () => { },
     } = {}
 ) {
     const timeoutIdRef = useRef<number | null>(null)
     useEffect(() => {
         let _stopped = false
-        ;(async function pollingCallback() {
-            try {
-                await asyncCallback()
-            } finally {
-                // Set timeout after it finished, unless stopped
-                timeoutIdRef.current = !_stopped && window.setTimeout(pollingCallback, interval)
-            }
-        })()
+            ; (async function pollingCallback() {
+                try {
+                    await asyncCallback()
+                } finally {
+                    // Set timeout after it finished, unless stopped
+                    timeoutIdRef.current = !_stopped && window.setTimeout(pollingCallback, interval)
+                }
+            })()
         // Clean up if dependencies change
         return () => {
             _stopped = true // prevent racing conditions
